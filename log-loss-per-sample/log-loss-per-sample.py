ac82583d79ac4bd0956d5ac94788dff82b02baf0
Nguyễn Tiến Thọ <121337540+tientho201@users.noreply.github.com>
2026-07-03 14:51:50 +0700

    Solve log-loss-per-sample

---
+++ log-loss-per-sample/log-loss-per-sample.py
@@ -0,0 +1,15 @@
+import math
+import numpy as np 
+
+def log_loss(y_true, y_pred, eps=1e-15):
+    """
+    Compute per-sample log loss.
+    """
+    # Write code here
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
+    
+    y_pred_clip = np.clip(y_pred , eps , 1 - eps)
+
+    loss = -(y_true * np.log(y_pred_clip) + (1 - y_true) * np.log(1 - y_pred_clip))
+    return loss.tolist()
